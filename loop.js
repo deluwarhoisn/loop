2c46471374d1952c174ea0b8bbab998e3f8632cd
deluwarhoisn <babuhossen301@gmail.com>
2025-08-02 18:42:01 +0600

    for js

---
+++ loop.js
@@ -0,0 +1,11 @@
+// const number= 'delwar hossin babu';
+
+// for(i=0; i<number.length; i++){
+//     console.log(number [i])
+// } 
+
+const name = 'deluwar hosin babu';
+for(i=0; i<name.length;i++){
+    console.log(name);
+}
+// console.log(number);
